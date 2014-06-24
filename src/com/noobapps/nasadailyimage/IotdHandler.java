984134fd0acba2dddf4ace0a3254d047553a6e62
demonic94 <himanshupandeylk@yahoo.com>
2014-06-25 00:07:00 +0530

    initial commit
    this is the first commit done when repo is created

---
+++ src/com/noobapps/nasadailyimage/IotdHandler.java
@@ -0,0 +1,130 @@
+package com.noobapps.nasadailyimage;
+
+
+/**
+ * IotdHandler
+ * This handler will collect the Image Of The Day
+ * 
+ */
+
+import org.xml.sax.helpers.DefaultHandler;
+import android.annotation.SuppressLint;
+import android.graphics.*;
+import android.os.StrictMode;
+
+import javax.xml.parsers.*;
+import org.xml.sax.*;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.HttpURLConnection;
+import java.io.IOException;
+
+@SuppressLint("NewApi")
+public class IotdHandler extends DefaultHandler {
+private String url = "http://www.nasa.gov/rss/image_of_the_day.rss";
+private boolean inUrl = false;
+private boolean inTitle = false;
+private boolean inDescription = false;
+private boolean inItem = false;
+private boolean inDate = false;
+private Bitmap image = null;
+private String imageUrl=null;
+private String title = null;
+private StringBuffer description = new StringBuffer();
+private String date = null;
+private int count=0;
+
+public void processFeed() {
+try {
+//This part is added to allow the network connection on a main GUI thread...    
+StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+StrictMode.setThreadPolicy(policy); 
+SAXParserFactory factory = SAXParserFactory.newInstance();
+SAXParser parser = factory.newSAXParser();
+XMLReader reader = parser.getXMLReader();
+reader.setContentHandler(this);
+URL urlObj = new URL(url);
+InputStream inputStream = urlObj.openConnection().getInputStream();
+reader.parse(new InputSource(inputStream));
+} 
+catch (Exception e) 
+{
+    e.printStackTrace();
+    System.out.println(new String("Got Exception General"));
+}
+}
+
+private Bitmap getBitmap(String url) {
+try {
+    System.out.println(url);
+HttpURLConnection connection =
+(HttpURLConnection)new URL(url).openConnection();
+connection.setDoInput(true);
+connection.connect();
+InputStream input = connection.getInputStream();
+Bitmap bitmap = BitmapFactory.decodeStream(input);
+input.close();
+return bitmap;
+} 
+catch (IOException ioe) 
+{
+    System.out.println(new String("IOException in reading Image"));
+    return null;
+}
+catch (Exception ioe) 
+{
+    System.out.println(new String("IOException GENERAL"));
+    return null;
+}
+}
+
+public void startElement(String uri, String localName, String qName,
+Attributes attributes) throws SAXException 
+{
+
+if (localName.equals("enclosure"))
+{
+    System.out.println(new String("characters Image"));
+    imageUrl = attributes.getValue("","url");
+    System.out.println(imageUrl);
+    inUrl = true; 
+}
+else { inUrl = false; }
+if (localName.startsWith("item")) { inItem = true;}
+else if (inItem) {
+if (localName.equals("title")) { inTitle = true; }
+else { inTitle = false; }
+if (localName.equals("description") && count<1) { inDescription = true; count++;}
+else { inDescription = false; }
+if (localName.equals("pubDate")) { inDate = true; }
+else { inDate = false; }
+}
+}
+
+public void characters(char ch[], int start, int length) {
+    System.out.println(new String("characters"));
+String chars = new String(ch).substring(start, start + length);
+System.out.println(chars);
+if (inUrl && image == null) 
+{
+    System.out.println(new String("IMAGE"));
+    System.out.println(imageUrl);
+    image = getBitmap(imageUrl);
+}
+if (inTitle && title == null) {
+    System.out.println(new String("TITLE"));
+    title = chars; }
+if (inDescription) { description.append(chars); }
+if (inDate && date == null) { date = chars; }
+}
+
+public Bitmap getImage() { return image; }
+public String getTitle() { return title; }
+public StringBuffer getDescription() { return description; }
+public String getDate() { return date; }
+
+}
+
---
+++ src/com/noobapps/nasadailyimage/MainActivity.java
@@ -0,0 +1,148 @@
+package com.noobapps.nasadailyimage;
+
+import java.io.IOException;
+
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.os.Handler;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.app.WallpaperManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+	private IotdHandler iotdHandler;
+	protected Bitmap image;
+	protected Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ConnectivityManager cm =
+                (ConnectivityManager)this.getSystemService(Context.CONNECTIVITY_SERVICE);
+         
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null &&
+                              activeNetwork.isConnectedOrConnecting();
+        if(isConnected == true){
+	        handler = new Handler();
+	        loadFeed();
+        }
+        else{
+        	AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        	alertDialog.setTitle("No Internet Connection");
+        	alertDialog.setMessage("no internet connection\napp will now exit");
+        	alertDialog.setNeutralButton("exit", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                	android.os.Process.killProcess(android.os.Process.myPid());
+                	System.exit(1);
+                } });
+            alertDialog.show();
+        	//android.os.Process.killProcess(android.os.Process.myPid());
+        	//System.exit(1);
+        }
+        
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+    
+    	switch(item.getItemId()){
+    	
+	    	case R.id.refresh : 
+	    		loadFeed();
+	    		return true;
+	    	
+	    	case R.id.setwall :
+			try {
+				setWallpaper(image);
+			} 
+			catch (IOException e) {
+				Toast.makeText(MainActivity.this, "@string/Tfail", Toast.LENGTH_SHORT).show();
+				e.printStackTrace();
+			}
+	    		return true;
+    	
+	    	default :
+	    		return super.onOptionsItemSelected(item);
+    	}
+    }
+    
+	private void resetDisplay(String title, String date,Bitmap image, StringBuffer description){
+    	TextView titleview = (TextView)findViewById(R.id.title);
+    	titleview.setText(title);
+    	TextView dateview = (TextView)findViewById(R.id.date);
+    	dateview.setText(date);
+    	ImageView imageview = (ImageView)findViewById(R.id.image);
+    	imageview.setImageBitmap(image);
+    	TextView desview = (TextView)findViewById(R.id.description);
+    	desview.setText(description);
+    }
+	
+	protected void loadFeed(){
+		
+		final ProgressDialog dialog = ProgressDialog.show(this,"Loading","Loading The Image");
+		Thread th = new Thread(){
+			public void run(){
+				if(iotdHandler == null)
+					iotdHandler = new IotdHandler();
+		        iotdHandler.processFeed();
+		        image = iotdHandler.getImage();
+		        handler.post(new Runnable(){
+		        	public void run(){
+		        		resetDisplay(iotdHandler.getTitle(), iotdHandler.getDate(),iotdHandler.getImage(), iotdHandler.getDescription());
+				        dialog.dismiss();
+			        } 	
+		        });
+			}
+		};
+		th.start();
+	}
+	
+	protected void setWallpaer(){
+		
+		Thread th1 = new Thread(){
+			
+			public void run(){
+				
+				WallpaperManager wm = WallpaperManager.getInstance(MainActivity.this);
+				try{
+					wm.setBitmap(image);
+					handler.post(new Runnable(){
+			        	public void run(){
+			        		Toast.makeText(MainActivity.this, "@string/Tsuccess", Toast.LENGTH_LONG).show();
+				        } 	
+			        });
+				}
+				catch(Exception e){
+					e.printStackTrace();
+					handler.post(new Runnable(){
+			        	public void run(){
+			        		Toast.makeText(MainActivity.this, "@string/Tfail", Toast.LENGTH_LONG).show();
+			        	}
+					});
+				}
+			}
+		};
+		th1.start();
+	}
+    
+}
